b7736cdb14abf0d83346e38d654fdf6a0f0f790c
Aiden Yi <huichanyi@gmail.com>
2018-12-02 23:03:55 -0800

    Implemented Posts to belong to Users

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,6 +3,13 @@
 require 'rails_helper'
 
 describe 'navigate' do
+
+  before do
+    user = User.create!(first_name: "Jon", last_name: "Snow", email: "test2@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+    login_as(user, :scope => :user)
+    visit new_post_path
+  end
+  
   describe 'index' do
     it 'can be reached successfully' do
       visit posts_path
@@ -16,11 +23,7 @@ describe 'navigate' do
   end
 
   describe 'creation' do
-    before do
-      user = User.create(first_name: "Jon", last_name: "Snow", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-      login_as(user, :scope => :user)
-      visit new_post_path
-    end
+
     it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
